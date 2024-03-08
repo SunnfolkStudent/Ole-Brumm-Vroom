67b55d4892fba96e5857d2c5d690457c671d2b49
secretarydum <>
2024-03-08 22:23:23 +0800

    Fixed DumSpawner

--- Assets/Scripts/DumSpawner.cs
+++ Assets/Scripts/DumSpawner.cs
@@ -9,11 +9,18 @@ public class DumSpawner : MonoBehaviour
     private Transform _transform;
     [SerializeField] private GameObject[] layoutPrefabs;
     private List<GameObject> _layoutInstances;
-    
+
+    [SerializeField] private Camera _cam;
+    private float camRightEdge;
     
     // Start is called before the first frame update
     void Start()
     {
+        _cam = Camera.main;
+        BoxCollider2D camBoundaries = _cam.GetComponent<BoxCollider2D>();
+        camRightEdge = camBoundaries.transform.position.x + (camBoundaries.bounds.size.x / 2);
+        
+        
         Parallax.OnRightEdgeView += EdgeReachView;
         Parallax.ReachEnd += EndReached;
         
@@ -30,9 +37,9 @@ public class DumSpawner : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Debug.DrawLine(new Vector3(camRightEdge, _transform.position.y), _transform.position, Color.black);
     }
 
     void InstantiateLayout(int indexOfLayout)
@@ -53,6 +60,14 @@ public class DumSpawner : MonoBehaviour
        
         
         _layoutInstances[x].SetActive(true);
+
+        float layoutWidth = _layoutInstances[x].GetComponent<BoxCollider2D>().bounds.size.x;
+        float positionX = camRightEdge + (layoutWidth / 2);
+        float positionY = this.transform.position.y;
+        _layoutInstances[x].transform.position = new Vector3(positionX, positionY);
+        
+        
+        Debug.DrawLine(_layoutInstances[x].transform.position, _transform.position, Color.yellow,2f);
     }
 
     void EndReached(int indexOfLayout)
