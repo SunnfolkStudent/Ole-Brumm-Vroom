35498d57e03d8f3bffde217579393c5bbe116040
Benjamin <benjamin.arctander@gmail.com>
2024-03-09 15:21:25 +0100

    Commenting out unused variables (removing warnings)

--- Assets/Scripts/ShaderManager.cs
+++ Assets/Scripts/ShaderManager.cs
@@ -6,25 +6,25 @@ public class ShaderManager : MonoBehaviour
     private static readonly int HoneyAmount = Shader.PropertyToID("_HoneyAmount");
     private static readonly int ColorStrength = Shader.PropertyToID("ColorStrength");
     
-    [SerializeField] private float honeyAmountPhase1 = 0f;
-    [SerializeField] private float honeyAmountPhase2 = 0.5f;
-    [SerializeField] private float honeyAmountPhase3 = 1.5f;
-    [SerializeField] private float honeyAmountPhase4 = 3f;
-    private float currentIncrementHoney;
-    private float currentIncrementColor;
+    // [SerializeField] private float honeyAmountPhase1 = 0f;
+    // [SerializeField] private float honeyAmountPhase2 = 0.5f;
+    // [SerializeField] private float honeyAmountPhase3 = 1.5f;
+    // [SerializeField] private float honeyAmountPhase4 = 3f;
+    private float _currentIncrementHoney;
+    private float _currentIncrementColor;
     private int _currentPhase;
     private float _currentHoneyAmount;
-    private float _nextHoneyAmount;
+    // private float _nextHoneyAmount;
     private float _currentColorStrength;
     private float _nextColorStrength;
-    private float _timeUntilNextPhase;
+    // private float _timeUntilNextPhase;
 
     // private float _startTime;
 
-    [SerializeField] private float colourStrengthPhase1 = 0f;
-    [SerializeField] private float colourStrengthPhase2 = 0.1f;
-    [SerializeField] private float colourStrengthPhase3 = 0.6f;
-    [SerializeField] private float colourStrengthPhase4 = 1f;
+    // [SerializeField] private float colourStrengthPhase1 = 0f;
+    // [SerializeField] private float colourStrengthPhase2 = 0.1f;
+    // [SerializeField] private float colourStrengthPhase3 = 0.6f;
+    // [SerializeField] private float colourStrengthPhase4 = 1f;
     
     // Start is called before the first frame update
     void Start()
@@ -39,8 +39,8 @@ public class ShaderManager : MonoBehaviour
 
     private void Update()
     {
-        _currentHoneyAmount += currentIncrementHoney;
-        _currentColorStrength += currentIncrementColor;
+        _currentHoneyAmount += _currentIncrementHoney;
+        _currentColorStrength += _currentIncrementColor;
         // _currentHoneyAmount += Mathf.Lerp(_currentHoneyAmount, _nextHoneyAmount, Time.time/_timeUntilNextPhase);
         // _currentColorStrength += Mathf.Lerp(_currentColorStrength, _nextColorStrength, Time.time/_timeUntilNextPhase);
         _spriteRenderer.material.SetFloat(HoneyAmount, _currentHoneyAmount);
@@ -52,47 +52,47 @@ public class ShaderManager : MonoBehaviour
         switch (phase)
         {
             case 1:
-                currentIncrementHoney = 0.00008f;
-                currentIncrementColor = 0.00008f;
+                _currentIncrementHoney = 0.00010f;
+                _currentIncrementColor = 0.00010f;
                 // _currentHoneyAmount = honeyAmountPhase1;
                 // _nextHoneyAmount = honeyAmountPhase2;
                 // _currentColorStrength = colourStrengthPhase1;
-                _nextColorStrength = colourStrengthPhase2;
-                _timeUntilNextPhase = 20f-1;
+                // _nextColorStrength = colourStrengthPhase2;
+                // _timeUntilNextPhase = 20f-1;
                 break;
             case 2:
-                currentIncrementHoney = 0.00015f;
-                currentIncrementColor = 0.00015f;
+                _currentIncrementHoney = 0.00010f;
+                _currentIncrementColor = 0.00010f;
                 // _currentHoneyAmount = honeyAmountPhase2;
                 // _nextHoneyAmount = honeyAmountPhase3;
                 // _currentColorStrength = colourStrengthPhase2;
-                _nextColorStrength = colourStrengthPhase3;
-                _timeUntilNextPhase = 20f+36f-1;
+                // _nextColorStrength = colourStrengthPhase3;
+                // _timeUntilNextPhase = 20f+36f-1;
                 break;
             case 3:
-                currentIncrementHoney = 0.00030f;
-                currentIncrementColor = 0.00030f;
+                _currentIncrementHoney = 0.00040f;
+                _currentIncrementColor = 0.00040f;
                 //_currentHoneyAmount = honeyAmountPhase3;
                 // _nextHoneyAmount = honeyAmountPhase4;
                 // _currentColorStrength = colourStrengthPhase3;
-                _nextColorStrength = colourStrengthPhase4;
-                _timeUntilNextPhase = 20f+36f+36f-1;
+                // _nextColorStrength = colourStrengthPhase4;
+                // _timeUntilNextPhase = 20f+36f+36f-1;
                 break;
             case 4:
-                currentIncrementHoney = 0.00040f;
-                currentIncrementColor = 0.00040f;
+                _currentIncrementHoney = 0.00080f;
+                _currentIncrementColor = 0.00080f;
                 //_currentHoneyAmount = honeyAmountPhase4;
                 // _nextHoneyAmount = honeyAmountPhase4;
                 // _currentColorStrength = colourStrengthPhase4;
-                _nextColorStrength = colourStrengthPhase4;
-                _timeUntilNextPhase = 20f+36f+36f+40f-1;
+                // _nextColorStrength = colourStrengthPhase4;
+                // _timeUntilNextPhase = 20f+36f+36f+40f-1;
                 break;
             case 5:
                 //_currentHoneyAmount = honeyAmountPhase1;
                 //_nextHoneyAmount = honeyAmountPhase2;
                 // _currentColorStrength = colourStrengthPhase1;
                 // _nextColorStrength = colourStrengthPhase2;
-                _timeUntilNextPhase = 1;
+                // _timeUntilNextPhase = 1;
                 break;
         }
     }
