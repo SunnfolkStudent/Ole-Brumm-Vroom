fc0b64ab51a85f9811a778bd0a60f838c81a4857
secretarydum <>
2024-03-09 22:47:39 +0800

    Cleaned code for DumSpawner

--- Assets/Scripts/DumSpawner.cs
+++ Assets/Scripts/DumSpawner.cs
@@ -127,74 +127,33 @@ public class DumSpawner : MonoBehaviour
             float positionX = _camRightEdge + (layoutWidth / 2);
             float positionY = DumSpawner._transform.position.y;
             _layoutInstancesNoObstacles[x].transform.position = new Vector3(positionX, positionY);
+            
+            
+            // obsPreset can't contain these:
+            // {1,2,3} --- 1, 2 and 3 can't be in the same preset
+            // {4,5,6}
+            // {7,8,9}
+        
+            int[] obsPreset1 = new[] { 1, 4, 7 }; // preset of PlatformLayout1
+            int[] obsPreset2 = new[] { 3, 6, 9 };
+            int[] obsPreset3 = new[] { 3, 6, 7 };
+            int[] obsPreset4 = new[] { 2, 5, 9 };
+            int[] obsPreset5 = new[] { 2, 5, 8 };
+            int[] obsPreset6 = new[] { 3, 4, 9 };
+            int[][] obsPresets = new int[][] { obsPreset1, obsPreset2, obsPreset3, obsPreset4, obsPreset5, obsPreset6  };
+        
+        
+            // int numberOfActiveObstacles = 0;
+        
+            int firstObsIndex = obsPresets[x][0];
+            int secondObsIndex= obsPresets[x][1];
+            int thirdObsIndex = obsPresets[x][2];
+        
+            SpawnObstacle(firstObsIndex);
+            SpawnObstacle(secondObsIndex);
+            SpawnObstacle(thirdObsIndex);
         }
-       
-        Debug.DrawLine(_layoutInstancesNoObstacles[x].transform.position, _transform.position, Color.yellow,2f);
-
-        // obsPreset can't contain these:
-        // {1,2,3} --- 1, 2 and 3 can't be in the same preset
-        // {4,5,6}
-        // {7,8,9}
         
-        int[] obsPreset1 = new[] { 1, 4, 7 }; // preset of PlatformLayout1
-        int[] obsPreset2 = new[] { 3, 6, 9 };
-        int[] obsPreset3 = new[] { 3, 6, 7 };
-        int[] obsPreset4 = new[] { 2, 5, 9 };
-        int[] obsPreset5 = new[] { 2, 5, 8 };
-        int[] obsPreset6 = new[] { 3, 4, 9 };
-        int[][] obsPresets = new int[][] { obsPreset1, obsPreset2, obsPreset3, obsPreset4, obsPreset5, obsPreset6  };
-        
-        
-        // int numberOfActiveObstacles = 0;
-        
-        int firstObsIndex = obsPresets[x][0];
-        int secondObsIndex= obsPresets[x][1];
-        int thirdObsIndex = obsPresets[x][2];
-        
-        SpawnObstacle(firstObsIndex);
-        SpawnObstacle(secondObsIndex);
-        SpawnObstacle(thirdObsIndex);
-        
-
-        // foreach (GameObject go in _obstacleInstances)
-        // {
-        //     if (go.activeSelf)
-        //     {
-        //         numberOfActiveObstacles++;
-        //     }
-        // }
-
-        // if (numberOfActiveObstacles < 2)
-        // {
-        //     int[] lane1 = new[] { 0, 1, 2 };
-        //     Random lr1 = new Random();
-        //     firstObsIndex = lane1[lr1.Next(0, lane1.Length - 1)];
-        //     SpawnObstacle(firstObsIndex+1);
-        //
-        //     numberOfActiveObstacles++;
-        // }
-        //
-        // if (numberOfActiveObstacles < 2)
-        // {
-        //     int[] lane2 = new[] { 3, 4, 5 };
-        //     Random lr2 = new Random();
-        //     secondObsIndex = lane2[lr2.Next(0, lane2.Length - 1)];
-        //     SpawnObstacle(secondObsIndex+1);
-        //     
-        //     numberOfActiveObstacles++;
-        // }
-        //
-        // if (numberOfActiveObstacles < 2)
-        // {
-        //     int[] lane3 = new[] { 6, 7, 8 };
-        //     Random lr3 = new Random();
-        //     thirdObsIndex = lane3[lr3.Next(0, lane3.Length - 1)];
-        //     SpawnObstacle(thirdObsIndex+1);
-        //     
-        //     numberOfActiveObstacles++;
-        // }
-        
-       
         
     }
 
